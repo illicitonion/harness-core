896308b572bafc35fed6d2508bbbfad135c0cf17
Raj Babu Das <rajbabu.das@harness.io>
2022-06-23 01:36:17 +0530

    feat: [CHAOS-42]: Adding Chaos audit event type
    Signed-off-by: Raj Babu Das <rajbabu.das@harness.io>

--- platform-service/modules/audit-service/contracts/src/main/java/io/harness/audit/beans/custom/chaos/ChaosAuditEventData.java
+++ platform-service/modules/audit-service/contracts/src/main/java/io/harness/audit/beans/custom/chaos/ChaosAuditEventData.java
@@ -5,33 +5,32 @@
  * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
  */
 
-package io.harness.audit.beans.custom.opa;
-
-import static io.harness.annotations.dev.HarnessTeam.OPA;
-import static io.harness.audit.beans.custom.AuditEventDataTypeConstants.OPA_AUDIT_EVENT_DATA;
-
-import io.harness.annotations.dev.OwnedBy;
-import io.harness.audit.beans.AuditEventData;
+package io.harness.audit.beans.custom.chaos;
 
 import com.fasterxml.jackson.annotation.JsonTypeName;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.audit.beans.AuditEventData;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.TypeAlias;
 
-@OwnedBy(OPA)
+import static io.harness.annotations.dev.HarnessTeam.CHAOS;
+import static io.harness.audit.beans.custom.AuditEventDataTypeConstants.CHAOS_AUDIT_EVENT_DATA;
+
+@OwnedBy(CHAOS)
 @Data
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-@JsonTypeName(OPA_AUDIT_EVENT_DATA)
-@TypeAlias(OPA_AUDIT_EVENT_DATA)
-public class OpaAuditEventData extends AuditEventData {
+@JsonTypeName(CHAOS_AUDIT_EVENT_DATA)
+@TypeAlias(CHAOS_AUDIT_EVENT_DATA)
+public class ChaosAuditEventData extends AuditEventData {
   String eventModule;
 
   @Builder
-  public OpaAuditEventData(String eventModule) {
+  public ChaosAuditEventData(String eventModule) {
     this.eventModule = eventModule;
-    this.type = OPA_AUDIT_EVENT_DATA;
+    this.type = CHAOS_AUDIT_EVENT_DATA;
   }
 }
